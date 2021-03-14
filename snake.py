a66bf614a7d0be3d1ce8a86d0f3f9b2a9508c0d2
Zeel <zeelp741@gmail.com>
2021-03-13 19:39:56 -0500

    added apple object

--- snake.py
+++ snake.py
@@ -50,3 +50,25 @@ class snake():
         self.snakelist.append(self.snakehead)
         if len(self.snakelist) > self.snakelength:
             del self.snakelist[0]
+
+class apple():
+    #return a (x,y)
+    def __init__(self, screenwidth, screenheight, gridsize):
+        self.screenwidth = screenwidth
+        self.screenheight = screenheight
+        self.gridsize = gridsize
+        self.x = round(random.randrange(0, self.screenwidth - self.gridsize)/self.gridsize)*self.gridsize
+        self.y = round(random.randrange(0, self.screenheight - self.gridsize)/self.gridsize)*self.gridsize
+    
+    def newloc(self, snake):
+        badspawn = True
+        while badspawn == True:
+            self.x = round(random.randrange(0, self.screenwidth - self.gridsize)/self.gridsize)*self.gridsize
+            self.y = round(random.randrange(0, self.screenheight - self.gridsize)/self.gridsize)*self.gridsize
+            badspawn = False
+            for position in snake.snakelist:
+                if position[0] == self.x and position[1] == self.y:
+                    badspawn = True
+                    break
+                else:
+                    badspawn = False
